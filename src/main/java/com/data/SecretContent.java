4a79fd068f2df47d1f698ecbfd9cc796a5621fd2
JohnsonWu <johnson760816@gmail.com>
2023-01-05 01:53:23 +0800

    1.Add read yaml file 2.Modify the db connection in java config file

---
+++ src/main/java/com/data/SecretContent.java
@@ -0,0 +1,11 @@
+package com.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class SecretContent {
+    private int mysqlDbPwd;
+    private String mixPrivateKey;
+}
